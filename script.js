cb75eb7f0f451b670c8c70e12141523971652aa7
Afnan Huda Amrullah <afnan.huda.ft21@mail.umy.ac.id>
2024-03-20 21:56:10 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+Date()
+let tanggal = new Date()
+document.getElementById("tanggal").innerHTML = tanggal
+function tampil(){
+    alert("ini adalah button")
+}
+function show(){
+    alert("inni adalah pop up untuk tulisan perkenalan")
+}
